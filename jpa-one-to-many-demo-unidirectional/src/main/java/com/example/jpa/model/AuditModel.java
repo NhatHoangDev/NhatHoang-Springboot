dc5309793fa85976dbf536040ca932601fccb834
NhatHoangDev <53230244+NhatHoangDev@users.noreply.github.com>
2021-05-27 16:49:14 +0700

    push

---
+++ jpa-one-to-many-demo-unidirectional/src/main/java/com/example/jpa/model/AuditModel.java
@@ -0,0 +1,19 @@
+package com.example.jpa.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+@Data
+@MappedSuperclass
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+public abstract class AuditModel {
+    @CreationTimestamp
+    private Date createdAt;
+    @UpdateTimestamp
+    private Date updatedAt;
+
+}
